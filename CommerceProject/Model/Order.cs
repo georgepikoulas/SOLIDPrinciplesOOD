17135a996ca435ad88cc4d2cc27bf68db22dacb0
George Pikoulas <pikoulas@fortunethellas.gr>
2020-11-02 16:02:48 +0200

    added Services and Utiity for srp

--- CommerceProject/Model/Order.cs
+++ CommerceProject/Model/Order.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net.Mail;
 using CommerceProject.Services;
 using CommerceProject.Utility;
---
+++ CommerceProject/Utility/Logger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommerceProject.Utility
+{
+    public static class Logger
+    {
+        public static void Error(string message, Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
